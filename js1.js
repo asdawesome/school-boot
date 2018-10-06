00458ac0765bbb3cd72331e2a737118358e272f2
asdawesome <42292991+asdawesome@users.noreply.github.com>
2018-10-06 21:15:36 +0300

    The starter look

---
+++ js1.js
@@ -0,0 +1,9 @@
+jQuery(document).on('click', '.mega-dropdown', function(e) {
+  e.stopPropagation()
+})
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
